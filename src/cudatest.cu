2e657fb954f3ed997814680707d385ddccb62969
Steven Lakin <lakinsm@rams.colostate.edu>
2016-07-10 12:17:28 -0600

    Testing event blocking

--- src/cudatest.cu
+++ src/cudatest.cu
@@ -26,23 +26,12 @@ int main() {
     float *F1, *F2, *d_F1, *d_F2;
 
     // Pinned memory for streaming; will have to account for dynamic size at some point
-    HANDLE_ERROR( cudaHostAlloc( (void**)&F1, k * kmer_count * NUM_READS * sizeof(*F1), cudaHostAllocDefault ) );
-    HANDLE_ERROR( cudaHostAlloc( (void**)&F2, k * kmer_count * NUM_READS * sizeof(*F2), cudaHostAllocDefault ) );
+    HANDLE_ERROR( cudaHostAlloc( (void**)&F1, k * kmer_count * sizeof(*F1), cudaHostAllocDefault ) );
+    HANDLE_ERROR( cudaHostAlloc( (void**)&F2, k * kmer_count * sizeof(*F2), cudaHostAllocDefault ) );
 
     // Device memory
-    HANDLE_ERROR( cudaMalloc( (void**)&d_F1, k * kmer_count * NUM_READS * sizeof(*d_F1) ) );
-    HANDLE_ERROR( cudaMalloc( (void**)&d_F2, k * kmer_count * NUM_READS * sizeof(*d_F2) ) );
-
-    // Fill F1 and F2 with data
-    for(unsigned long n = 0; n < NUM_READS; ++n) {
-        // Fill F1 and F2 with data
-        for (int m = 0; m < kmer_count; ++m) {
-            for (int b = 0; b < k; ++b) {
-                F1[n * ((m * k) + b) + ((m * k) + b)] = (float) r1[m + b] - '0';
-                F2[n * ((m * k) + b) + ((m * k) + b)] = (float) r2[m + b] - '0';
-            }
-        }
-    }
+    HANDLE_ERROR( cudaMalloc( (void**)&d_F1, k * kmer_count * sizeof(*d_F1) ) );
+    HANDLE_ERROR( cudaMalloc( (void**)&d_F2, k * kmer_count * sizeof(*d_F2) ) );
 
     ////////////////////
     // Other Matrices //
@@ -50,8 +39,8 @@ int main() {
     // Allocate memory on host
     float *T, *R1, *R2, *d_T, *d_R1, *d_R2;
     T = (float*)std::malloc(k * T_cols * sizeof(*T));
-    HANDLE_ERROR( cudaHostAlloc( (void**)&R1, kmer_count * NUM_READS * T_cols * sizeof(*R1), cudaHostAllocDefault ) );
-    HANDLE_ERROR( cudaHostAlloc( (void**)&R2, kmer_count * NUM_READS * T_cols * sizeof(*R2), cudaHostAllocDefault ) );
+    HANDLE_ERROR( cudaHostAlloc( (void**)&R1, kmer_count * T_cols * sizeof(*R1), cudaHostAllocDefault ) );
+    HANDLE_ERROR( cudaHostAlloc( (void**)&R2, kmer_count * T_cols * sizeof(*R2), cudaHostAllocDefault ) );
 
     // Device alloc
     HANDLE_ERROR( cudaMalloc( &d_T, k * T_cols * sizeof(*d_T) ) );
@@ -72,16 +61,16 @@ int main() {
     std::free(T);
 
     // Choose device
-    cudaSetDevice(0);
+    cudaSetDevice(1);
 
     /* Usually the below would be something like this:
      * int N_THREADS_PER_BLOCK = 256; (or 16x16, 32x8 for 2D)
      * int N_BLOCKS_PER_GRID = (N + threadsPerBlock - 1) / threadsPerBlock; for 1D
      */
     std::cout << "Input dimensions:" << std::endl;
-    std::cout << "Number of elements in features: " << 2* k * kmer_count * NUM_READS  << std::endl;
-    std::cout << "Number of elements in training: " << k * T_cols  << std::endl;
-    std::cout << "Number of elements in result: " << kmer_count * T_cols  << std::endl;
+    std::cout << "Number of total elements in feature arrays: " << 2 * k * kmer_count << std::endl;
+    std::cout << "Number of elements in training array: " << k * T_cols  << std::endl;
+    std::cout << "Number of elements in result array: " << kmer_count * T_cols  << std::endl;
 
     dim3 dimBlock(32, 8);  // Based on comments on StackOverflow for 2D threads
     dim3 dimGrid(T_cols / dimBlock.x, kmer_count / dimBlock.y);
@@ -104,28 +93,49 @@ int main() {
     if(!prop.deviceOverlap)
         std::cout << "Device streaming overlap disabled, therefore no speedup expected" << std::endl;
 
-    // Initialize streams
+    // Initialize streams and copy event markers
     cudaStream_t stream0, stream1;
+    cudaEvent_t cp0, cp1;
     HANDLE_ERROR( cudaStreamCreate( &stream0 ) );
     HANDLE_ERROR( cudaStreamCreate( &stream1 ) );
+    HANDLE_ERROR( cudaEventCreateWithFlags( &cp0, cudaEventDisableTiming ) );
+    HANDLE_ERROR( cudaEventCreateWithFlags( &cp1, cudaEventDisableTiming ) );
 
-    for (unsigned long i = 0; i < k * kmer_count * NUM_READS; i += k * kmer_count) {
-        // Enque the memory streams in breadth-first order such that
-        // the block scheduler launches kernels optimally
-        HANDLE_ERROR(cudaMemcpyAsync(d_F1, F1 + i, k * kmer_count * NUM_READS * sizeof(float),
-                                     cudaMemcpyHostToDevice, stream0));
-        HANDLE_ERROR(cudaMemcpyAsync(d_F2, F2 + i, k * kmer_count * NUM_READS * sizeof(float),
-                                     cudaMemcpyHostToDevice, stream1));
+    for(unsigned long r = 0; r < NUM_READS; ++r) {
+        // Fill F1 and F2 with new data
+        for (int m = 0; m < kmer_count; ++m) {
+            for (int b = 0; b < k; ++b) {
+                F1[(m * k) + b] = (float) r1[m + b] - '0';
+                F2[(m * k) + b] = (float) r2[m + b] - '0';
+            }
+        }
 
-        // Enque the kernel launches
-        MatHamm <<< dimGrid, dimBlock, 0, stream0 >>>(d_F1, d_T, d_R1, kmer_count, k, k, T_cols, kmer_count, T_cols);
-        MatHamm <<< dimGrid, dimBlock, 0, stream1 >>>(d_F2, d_T, d_R2, kmer_count, k, k, T_cols, kmer_count, T_cols);
+        for (unsigned long i = 0; i < k * kmer_count; i += k * kmer_count) {
+            // Enque the memory streams in breadth-first order such that
+            // the block scheduler launches kernels optimally
+            HANDLE_ERROR(cudaMemcpyAsync(d_F1, F1 + i, k * kmer_count * sizeof(float),
+                                         cudaMemcpyHostToDevice, stream0));
+            HANDLE_ERROR(cudaMemcpyAsync(d_F2, F2 + i, k * kmer_count * sizeof(float),
+                                         cudaMemcpyHostToDevice, stream1));
 
-        // Enque copy back to host
-        HANDLE_ERROR(cudaMemcpyAsync(R1 + i, d_R1, k * kmer_count * NUM_READS * sizeof(float),
-                                     cudaMemcpyDeviceToHost, stream0));
-        HANDLE_ERROR(cudaMemcpyAsync(R2 + i, d_R2, k * kmer_count * NUM_READS * sizeof(float),
-                                     cudaMemcpyDeviceToHost, stream1));
+            // Insert event markers into the stream to tell host
+            // when F1 and F2 are safe to overwrite in host memory
+            HANDLE_ERROR( cudaEventRecord( &cp0, stream0 ) );
+            HANDLE_ERROR( cudaEventRecord( &cp1, stream1 ) );
+
+            // Enque the kernel launches
+            MatHamm <<< dimGrid, dimBlock, 0, stream0 >>> (d_F1, d_T, d_R1, kmer_count, k, k, T_cols, kmer_count, T_cols);
+            MatHamm <<< dimGrid, dimBlock, 0, stream1 >>> (d_F2, d_T, d_R2, kmer_count, k, k, T_cols, kmer_count, T_cols);
+
+            // Enque copy back to host
+            HANDLE_ERROR(cudaMemcpyAsync(R1 + i, d_R1, k * kmer_count * sizeof(float),
+                                         cudaMemcpyDeviceToHost, stream0));
+            HANDLE_ERROR(cudaMemcpyAsync(R2 + i, d_R2, k * kmer_count * sizeof(float),
+                                         cudaMemcpyDeviceToHost, stream1));
+        }
+        // Block host from proceeding until copy to GPU is complete
+        HANDLE_ERROR( cudaEventSynchronize( cp0 ) );
+        HANDLE_ERROR( cudaEventSynchronize( cp1 ) );
     }
 
     // Synchronize to ensure work is complete
@@ -146,6 +156,8 @@ int main() {
     HANDLE_ERROR( cudaFree(d_R2) );
     HANDLE_ERROR( cudaStreamDestroy( stream0 ) );
     HANDLE_ERROR( cudaStreamDestroy( stream1 ) );
+    HANDLE_ERROR( cudaEventDestroy( cp0 ) );
+    HANDLE_ERROR( cudaEventDestroy( cp1 ) );
 
 
     // Output to test results
